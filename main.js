0b87e98c80c594e149569bf3b4a674d6eff4b387
ViNu-23 <vijayvinu46@gmail.com>
2023-11-28 22:15:15 +0530

    display api data in body

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 // Fetching data from local text file using promise
 
-// fetch('data.txt').then(function(result){
+// fetch("data.txt")
+//   .then(function (result) {
 //     return result.text();
-// }).then(function(data){
-//     console.log(data);
-// }).catch(function(error){
+//   })
+//   .then(function (data) {
+//     const paraGraph = document.querySelector(".api-data");
+//     paraGraph.textContent = data;
+//   })
+//   .catch(function (error) {
 //     console.log(error);
-// });
+//   });
 
 // Fetching data from local json file using promise
 
@@ -49,18 +53,17 @@
 // }
 // getTextData();
 
-
 // Fetching data from URL link using async and await
- 
-async function getTextData() {
-    try {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users");
-      const data = await response.json();
-      for (let i = 0; i < data.length; i++) {
-        console.log(`Name:${data[i].name} & User Name: ${data[i].username}`);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  getTextData();
+
+// async function getTextData() {
+//     try {
+//       const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//       const data = await response.json();
+//       for (let i = 0; i < data.length; i++) {
+//         console.log(`Name:${data[i].name} & User Name: ${data[i].username}`);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+//   getTextData();
