8e8c9331b5c75604c8388683adec98858db106f7
ViNu-23 <vijayvinu46@gmail.com>
2023-11-28 22:04:21 +0530

    url fetch

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-//Fetch API using promise, async and await in JS
-
 // Fetching data from local text file using promise
+
 // fetch('data.txt').then(function(result){
 //     return result.text();
 // }).then(function(data){
 //     console.log(data);
 // }).catch(function(error){
 //     console.log(error);
+// });
+
+// Fetching data from local json file using promise
+
+// fetch('data.json')
+//  .then(function(result){
+//     return result.json();
+// }).then(function(data){
+//     for(let i=0;i<data.length;i++){
+//         console.log(`Name:${ data[i].name } Age: ${data[i].age}`);
+//     }
+// }).catch(function(error){
+//     console.log(error);
 // })
 
 //Fetching data from local text file using async and await
+
 // async function getTextData(){
 //    try {
 //     const response = await fetch('data.txt');
@@ -21,26 +34,33 @@
 // }
 // getTextData();
 
+// Fetching data from local json file using async and await
 
-// Fetching data from local json file using promise
-fetch('data.json').then(function(result){
-    return result.text();
-}).then(function(data){
-    console.log(data);
-}).catch(function(error){
-    console.log(error);
-})
-
-
-
-//Fetching data from local json file using async and await
-// async function getTextData(){
-//     try {
-//      const response = await fetch('data.json');
-//      const data = await response.text();
-//      console.log(data);
-//     } catch (error) {
-//      console.log(error);
+// async function getTextData() {
+//   try {
+//     const response = await fetch("data.json");
+//     const data = await response.json();
+//     for (let i = 0; i < data.length; i++) {
+//       console.log(`Name:${data[i].name} Age: ${data[i].age}`);
 //     }
-//  }
-//  getTextData();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// getTextData();
+
+
+// Fetching data from URL link using async and await
+ 
+async function getTextData() {
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await response.json();
+      for (let i = 0; i < data.length; i++) {
+        console.log(`Name:${data[i].name} & User Name: ${data[i].username}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  getTextData();
